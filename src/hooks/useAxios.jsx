1fe631af3c9321524971cd90611ced3dee66bb0e
mashcatg <mashcatg@gmail.com>
2025-04-24 09:29:29 +0600

    mash

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("auth"))
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://ayotto-test-server.vercel.app/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://ayotto-test-server.vercel.app/api",
   // withCredentials: true,
   headers:{
     Authorization: `${token?.token}`
